1c71836853ca3faa2c99911bcc1041f44c2c5326
Klopokayzer <liber150395@gmail.com>
2021-03-25 22:15:27 +0300

    update dz

--- urok7.c
+++ urok7.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+int median(int a, int b, int c){
+	if (a > b){
+		if (b > c)
+		return b;
+		else if (a > c)
+		return c;
+		else 
+		return a;
+	}else{
+		if (a > c)
+		return a;
+		else if (b > c)
+		return c;
+		else
+		return b;
+	}
+}
+
 void sortInserts(int* arr, int len){
 	int temp, pos;
 	for(int i = 1; i < len; ++i){
@@ -50,32 +68,36 @@ void QiuckSort(int* arr, int first, int last){
 	if (first < j) QiuckSort(arr, first, j);
 }
 
-/*void BlockSort(int* arr, int len){
+void BlockSort(int* arr, int len){
 	const int max = len;
 	const int b = 10;
-	int buckets[b];
-	for(int i=0; i < b; ++i){
-		if(arr[i]%2==0){
-			int cont = buckets[i];
-			buckets[arr[i]%b] =  arr[i];
-			
-		}
+	int buckets[b][max + 1];
+	for (int i = 0; i < b; i++){
+		buckets[i][max] = 0;
+	}
+	
+	for(int digit = 1; digit < 100; digit*=10){
+		for(int i =0; i < max; i++){
+			if(arr[i]%2==0){
 			int d = (arr[i]/digit)%b;
 			int counter = buckets[d][max];
 			buckets[d][counter] = arr[i];
 			counter++;
 			buckets[d][max]=counter;
+			arr[i]= -1;
+			}
 		}
 		int idx = 0;
 		for(int i =0; i < b; ++i){
-			if(arr[i]%2==0){
 			for(int j = 0; j < buckets[i][max]; ++j){
+				while (arr[idx] != -1)
+				idx++;
 				arr[idx++]=buckets[i][j];
 			}
 			buckets[i][max]=0;
 		}
 	}
-}*/
+}
 
 int main(){
 	const int SIZE = 30;
@@ -87,7 +109,7 @@ int main(){
 	if(SIZE<=10){
 		sortInserts(array, SIZE);
 	}else{
-		a = (array[0]+array[SIZE/2]+array[SIZE-1])/3;
+		a = median(array[0],array[SIZE/2],array[SIZE-1]);
 		array[SIZE/2] = a;
 		QiuckSort(array, 0, SIZE-1);
 	}
@@ -96,8 +118,9 @@ int main(){
 	//2 задание
 	int arr2[12] = {0, 2, 8, 3, 4, 6, 5, 9, 8, 2, 7, 3};
 	printIntArray(arr2, 12);
-	/*BlockSort(arr2, 12);
-	printIntArray(arr2, 12);*/
-	//Не смог разобраться с блочной сортировкой, чтобы она поменяла всего 2 числа местами... Получилось все четные числа сделать 2 и 8))...
+	BlockSort(arr2, 12);
+	printIntArray(arr2, 12);
+	//Не смог разобраться с блочной сортировкой, чтобы она поменяла всего 2 числа местами... 
+	//Получилось все четные числа сделать 2 и 8))...
 	return 0;
 }
