f778f7c9b44656c086d4103aab7d8204932b1d97
Klopokayzer <liber150395@gmail.com>
2021-06-24 16:19:29 +0300

    Добавлено дз для 1 урока

---
+++ OOP/1 urok.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <math.h>
+#include <cstdint>
+
+// Task 1
+class Power
+{
+private:
+    int m_num1;
+    int m_num2;
+
+public:
+    void setnum(int num1, int num2)
+    {
+        m_num1 = num1;
+        m_num2 = num2;
+    }
+    int calculate()
+    {
+        return pow(m_num1, m_num2);
+    }
+};
+// Task 2
+class RGBA
+{
+private:
+    std::uint8_t m_red = 0;
+    std::uint8_t m_green = 0;
+    std::uint8_t m_blue = 0;
+    std::uint8_t m_alpha = 255;
+
+public:
+    RGBA()
+    {
+    }
+
+    RGBA(int red, int green, int blue, int alpha)
+        : m_red(red), m_green(green), m_blue(blue), m_alpha(alpha)
+    {
+    }
+    void print()
+    {
+        std::cout << (int)m_red << ", " << (int)m_green << ", " << (int)m_blue << ", " << (int)m_alpha << std::endl;
+    }
+};
+// Task 3
+class Stack
+{
+    private:
+        int stack[10];
+        int key = 0;
+    public:
+        bool push(int a){
+            if(key <= 9){
+                stack[key] = a;
+                key++;
+                return true;
+            } else {
+                std::cout<<"Stack owerflow!"<<std::endl;
+                return false;
+            }
+        }
+        void print(){
+            std::cout<<"( ";
+            for(int i=0;i<=key;i++){
+                std::cout<<stack[i]<<", ";
+            }
+            std::cout<<")"<<std::endl;
+        }
+};
+int main()
+{
+    //Task 1
+    Power power;
+    power.setnum(5, 2);
+    std::cout << power.calculate() << std::endl;
+
+    //Task 2
+    RGBA RGBA1;
+    RGBA1.print();
+    int red;
+    std::cin >> red;
+    int green;
+    std::cin >> green;
+    int blue;
+    std::cin >> blue;
+    int alpha;
+    std::cin >> alpha;
+    RGBA RGBA2(red, green, blue, alpha);
+    RGBA2.print();
+
+    // Task 3
+    Stack stack;
+    stack.reset();
+    stack.print();
+}
